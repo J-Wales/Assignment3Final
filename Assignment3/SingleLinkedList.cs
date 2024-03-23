6099284d1fe5d39da5e1b3d6f035a5f7558d0a74
J-Wales <j.wales@edu.sait.ca>
2024-03-23 16:42:04 -0600

    Added skeleton for the SingleLinkedList class.

---
+++ Assignment3/SingleLinkedList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+	internal class SingleLinkedList<T> : ILinkedListADT<T>
+	{
+		public Node<T> Head { get; protected set; }
+		public Node<T> Tail {  get; protected set; }
+
+		public int count {  get; protected set; }
+
+		public bool IsEmpty()
+		{
+
+		}
+
+		public void Clear()
+		{
+
+		}
+
+		public void AddLast(User value)
+		{
+
+		}
+
+		public void AddFirst(User value)
+		{
+
+		}
+
+		public void Add(User value, int index)
+		{
+
+		}
+
+		public void Replace(User value, int index)
+		{
+
+		}
+
+		public int Count()
+		{
+			return count;
+		}
+
+		public void RemoveFirst()
+		{
+
+		}
+
+		public void RemoveLast()
+		{
+
+		}
+
+		public void Remove(int index)
+		{
+
+		}
+
+		public User GetValue(int index)
+		{
+
+		}
+
+		public int IndexOf(User value)
+		{
+
+		}
+
+		public bool Contains(User value)
+		{
+
+		}
+	}
+}
--- Assignment3/Utility/ILinkedListADT.cs
+++ Assignment3/Utility/ILinkedListADT.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Assignment3
 {
-    public interface ILinkedListADT
+    public interface ILinkedListADT<T>
     {
         /// <summary>
         /// Checks if the list is empty.
