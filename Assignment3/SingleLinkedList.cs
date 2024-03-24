545138cc5ccc325bb3f49e4b08be39ef5eca1b69
SatkiratV2 <Satkirat66@gmail.com>
2024-03-23 20:52:48 -0600

    Added remove at index method

--- Assignment3/SingleLinkedList.cs
+++ Assignment3/SingleLinkedList.cs
@@ -8,8 +8,8 @@ namespace Assignment3
 {
 	internal class SingleLinkedList<T> : ILinkedListADT<T>
 	{
-		public Node<T> Head { get; protected set; }
-		public Node<T> Tail {  get; protected set; }
+		public Node<T> head { get; protected set; }
+		public Node<T> tail {  get; protected set; }
 
 		public int count {  get; protected set; }
 
@@ -85,8 +85,26 @@ namespace Assignment3
 
 		public void Remove(int index)
 		{
-
-		}
+			//If index points to start of list, call remove first method
+			if (index == 0)
+			{
+				RemoveFirst();
+			}
+			else
+			{
+				var currentNode = head;
+				//Iterates through the linked list
+				for (int i = 0; i < index - 1; i++)
+				{
+					currentNode = currentNode.Next;
+				}
+				//creates a var to hold the node that is gonna be removed
+				var nodeToRemove = currentNode.Next;
+				currentNode.Next = nodeToRemove.Next;
+				//Sets the previous node from the removed node as the new tail
+				tail = currentNode;
+				
+			}
 
 		public User GetValue(int index)
 		{
